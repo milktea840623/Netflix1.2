8d181c412966c330302de726ef8d49288d96e992
milktea840623 <s9000070@gmail.com>
2025-02-21 18:35:49 +0800

    0221 end

--- src/Header.jsx
+++ src/Header.jsx
@@ -13,7 +13,6 @@ function Header() {
                 setIsScrolled(false);
             }
         };
-
         window.addEventListener('scroll', handleScroll);
         return () => {
             window.removeEventListener('scroll', handleScroll);
--- src/VideoSection.jsx
+++ src/VideoSection.jsx
@@ -3,8 +3,8 @@ import style from './VideoSection.module.css'
 
 const VideoSection = () => {
   const videoRef = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [isMuted, setIsMuted] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [isMuted, setIsMuted] = useState(true);
 
   const togglePlayPause = () => {
     if (isPlaying) {
@@ -22,21 +22,36 @@ const VideoSection = () => {
 
   return (
     <div className={style.videoSection}>
-      <video ref={videoRef} className={style.video} loop muted>
-        <source src="../public/video/13111085_360_640_25fps.mp4" type="video/mp4" />
+      <video ref={videoRef} className={style.video} loop autoPlay muted={isMuted}>
+        <source src="/video/13111085_360_640_25fps.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <div className={style.shadowOverlay}>
+      </div>
       <div className={style.overlay}>
-        <h1>影片介紹</h1>
-        <p>這是一段影片的介紹文字。</p>
-        <button className={style.button} onClick={togglePlayPause}>
-          {isPlaying ? '暫停' : '播放'}
-        </button>
-        <button className={style.button} onClick={toggleMute}>
-          {isMuted ? '開啟聲音' : '靜音'}
-        </button>
+        <div className={style.textBlock}>
+          <h1>🍕 美味時刻</h1>
+          <h4>誘人的起司拉絲，每一口都是幸福的滋味！</h4>
+          <p>新鮮出爐的披薩，酥脆外皮搭配濃郁起司與豐富配料，香氣四溢，每一片都是完美享受。<br />無論是派對、聚會還是獨享時光，這一刻就該好好品味！🍕✨</p>
+          <div className={style.moreInfoBlock}>
+            <button className={`${style.button} ${style.playButton}`} onClick={togglePlayPause}>
+              {isPlaying ? '暫停' : '播放'}
+            </button>
+            <button className={`${style.button} ${style.moreInfoButton}`}>更多資訊</button>
+          </div>
+        </div>
+        <div className={style.RBButtonBlock}>
+          <div className={style.buttonBlock}>
+            <button className={style.button} onClick={toggleMute}>
+              {isMuted ? '開啟聲音' : '靜音'}
+            </button>
+          </div>
+          <div className={style.ageRating}>18+
+          </div>
+        </div>
       </div>
     </div>
+
   );
 };
 
