e529772828dcbfc2dee2f9836b98fff65086c7e1
milktea840623 <s9000070@gmail.com>
2025-02-24 17:46:00 +0800

    0204 end

--- src/VideoSection.jsx
+++ src/VideoSection.jsx
@@ -1,18 +1,23 @@
 import React, { useRef, useState } from 'react';
 import style from './VideoSection.module.css'
+import ClickIconMedia from './assets/image/play_pause_icon_137298.png';
+import ClickIconMoreInfo from './assets/image/infor sign.png';
+import ClickIconMute from './assets/image/mute_106517.png';
+import ClickIconUnmute from './assets/image/sound.png';
+import ClickIconRightArrow from './assets/image/right-arrow.png';
 
 const VideoSection = () => {
   const videoRef = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(false);
   const [isMuted, setIsMuted] = useState(true);
 
   const togglePlayPause = () => {
-    if (isPlaying) {
-      videoRef.current.pause();
-    } else {
-      videoRef.current.play();
-    }
     setIsPlaying(!isPlaying);
+    if (isPlaying) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
   };
 
   const toggleMute = () => {
@@ -26,7 +31,9 @@ const VideoSection = () => {
         <source src="/video/13111085_360_640_25fps.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className={style.shadowOverlay}>
+      <div className={style.leftShadowOverlay}>
+      </div>
+      <div className={style.buttomShadowOverlay}>
       </div>
       <div className={style.overlay}>
         <div className={style.textBlock}>
@@ -35,14 +42,24 @@ const VideoSection = () => {
           <p>新鮮出爐的披薩，酥脆外皮搭配濃郁起司與豐富配料，香氣四溢，每一片都是完美享受。<br />無論是派對、聚會還是獨享時光，這一刻就該好好品味！🍕✨</p>
           <div className={style.moreInfoBlock}>
             <button className={`${style.button} ${style.playButton}`} onClick={togglePlayPause}>
-              {isPlaying ? '暫停' : '播放'}
+              <img src={ClickIconMedia} alt="播放圖示" className={style.iconMedia} />
+              {isPlaying ? '播放' : '暫停'}
             </button>
-            <button className={`${style.button} ${style.moreInfoButton}`}>更多資訊</button>
+            <button className={`${style.button} ${style.moreInfoButton}`}>
+              <img src={ClickIconMoreInfo} alt="更多資訊圖示" className={style.iconMoreInfo} />
+              更多資訊
+            </button>
           </div>
+        </div>
+        <div className={style.recommendation}>
+          <button className={style.dailyBestComButton}>本日最佳推薦</button>
+          <div className={style.rightArrow}></div>
+          <div className={style.exploreButton}>探索全部</div>
         </div>
         <div className={style.RBButtonBlock}>
           <div className={style.buttonBlock}>
-            <button className={style.button} onClick={toggleMute}>
+            <button className={`${style.button} ${style.muteButton}`} onClick={toggleMute}>
+              <img src={isMuted ? ClickIconMute : ClickIconUnmute} alt="聲音圖示" className={style.iconMute} />
               {isMuted ? '開啟聲音' : '靜音'}
             </button>
           </div>
